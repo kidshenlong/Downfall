1e974dd73e15eba3d5422170a986780a81c4a210
kidshenlong <michaelpm91@googlemail.com>
2013-10-30 14:55:50 +0000

    Head Bobber Script
    Added basic Head Bobber Script for somewhat more accurate movement. Also
    a random start on new Movement Script

---
+++ Assets/Scripts/HeadBobberScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadBobberScript : MonoBehaviour {
+	
+	private float timer= 0.0f; 
+	public float bobbingSpeed= 0.18f; 
+ 	public float bobbingAmount= 0.2f; 
+ 	public float midpoint= 2.0f;
+	float waveslice;
+	float horizontal;
+	float vertical;
+	float translateChange;
+	float totalAxes;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		waveslice = 0.0f; 
+	    horizontal = Input.GetAxis("Horizontal"); 
+	    vertical = Input.GetAxis("Vertical"); 
+	    if (Mathf.Abs(horizontal) == 0 && Mathf.Abs(vertical) == 0) { 
+	       timer = 0.0f; 
+	    } 
+	    else { 
+	       waveslice = Mathf.Sin(timer); 
+	       timer = timer + bobbingSpeed; 
+	       if (timer > Mathf.PI * 2) { 
+	          timer = timer - (Mathf.PI * 2); 
+	       } 
+	    } 
+	    if (waveslice != 0) { 
+	       translateChange = waveslice * bobbingAmount; 
+	       totalAxes = Mathf.Abs(horizontal) + Mathf.Abs(vertical); 
+	       totalAxes = Mathf.Clamp (totalAxes, 0.0f, 1.0f); 
+	       translateChange = totalAxes * translateChange; 
+	       //transform.localPosition.y = midpoint + translateChange; 
+		   transform.localPosition = new Vector3( 0, midpoint + translateChange,0); 
+	    } 
+	    else { 
+	       //transform.localPosition.y = midpoint;
+		   transform.localPosition = new Vector3( 0, midpoint,0);	
+	    } 
+	 }
+	
+}
+
---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour {
+	
+	//public enum Direction { Forward = 0, Right = 1, Left = 2, Back = 3 }
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		if (Input.GetKey ("w")){
+			rigidbody.AddForce (10,0,0);
+		}
+		if (Input.GetKey ("d")){
+			rigidbody.AddForce (0,0,-10);
+		}
+		if (Input.GetKey ("s")){
+			rigidbody.AddForce (-10,0,0);
+		}
+		if (Input.GetKey ("a")){
+			rigidbody.AddForce (0,0,10);
+		}
+	
+	}
+}
