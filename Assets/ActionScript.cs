6e317ff40f444f7c28606c0f80a6384c11ffb82c
kidshenlong <michaelpm91@googlemail.com>
2013-10-30 17:16:46 +0000

    Implemented Basic Weapon!
    Very basic gun/rocket launcher.

---
+++ Assets/ActionScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionScript : MonoBehaviour {
+	
+	public Rigidbody bulletPrefab;
+	public Transform barrelEnd;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetButtonDown("Fire1"))
+		{
+			Rigidbody bulletInstance;
+			bulletInstance = Instantiate(bulletPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+			bulletInstance.AddForce(barrelEnd.forward * 5000);
+		}	
+	
+	}
+	
+	void Shoot(){
+		/*Rigidbody bulletInstance;
+		bulletInstance = Instantiate(bulletPrefab, barrelEnd.position, barrelEnd.rotation) as Rigidbody;
+		//bulletInstance.AddForce(0,0,5000);
+		//bulletInstance.AddForce(barrelEnd.forward * 5000);
+		bulletInstance.AddForce(barrelEnd.forward * 5000);*/
+	}
+}
---
+++ Assets/BulletDestructionScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletDestructionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		 Destroy (gameObject, 3.5f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter (Collision col) {
+		
+		Destroy (gameObject, 1.0f);
+		
+	}
+}
